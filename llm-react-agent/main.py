83eb34d88c29b18263624e1232d80fb825cd1843
MaheshBiradar <bmahesh66@gmail.com>
2025-02-09 21:33:02 -0500

    Added react tooling using agents

---
+++ llm-react-agent/main.py
@@ -0,0 +1,100 @@
+from dotenv import load_dotenv
+from typing import Union, List
+from langchain.agents import Tool, tool
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain_core.agents import AgentAction, AgentFinish
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import render_text_description
+from langchain_openai import ChatOpenAI
+from langchain.agents.format_scratchpad import format_log_to_str
+
+load_dotenv()
+
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f"Hi, {name}")  # Press ⌘F8 to toggle the breakpoint.
+
+
+@tool
+def get_text_length(text: str) -> int:
+    """Get the length of a text."""
+    text = text.strip("'\n").strip('"')
+    return len(text)
+
+
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool wtih name {tool_name} not found")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == "__main__":
+    print_hi("PyCharm")
+    print(get_text_length.invoke(input={"text": "Hello world!"}))
+    tools = [get_text_length]
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+    
+    Use the following format:
+    
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+    
+    Begin!
+    
+    Question: {input}
+    Thought:{agent_scratchpad}
+    """
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools),
+        tool_names=",".join([tool.name for tool in tools]),
+    )
+
+    llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation"])
+    intermediate_steps = []
+    agent = (
+        {
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"]),
+        }
+        | prompt
+        | llm
+        | ReActSingleInputOutputParser()
+    )
+    agent_step = ""
+    while not isinstance(agent_step, AgentFinish):
+        agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+            {
+                "input": "What is the length of 'Mahesh' in characters?",
+                "agent_scratchpad": intermediate_steps,
+            }
+        )
+        print(agent_step)
+
+        if isinstance(agent_step, AgentAction):
+            tool_name = agent_step.tool
+            tool_to_use = find_tool_by_name(tools, tool_name)
+            tool_input = agent_step.tool_input
+
+            observation = tool_to_use.func(str(tool_input))
+            print(f"{observation=}")
+            intermediate_steps.append((agent_step, str(observation)))
+
+    if isinstance(agent_step, AgentFinish):
+        print(agent_step.return_values)
